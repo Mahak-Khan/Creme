90e9a822e6d6752e9cca8c2be52ff50bb4bd7080
Mahak <mahaksinghkhan@gmail.com>
2025-12-12 12:04:36 +0530

    Config Changes

--- react/vite.config.js
+++ react/vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// ✅ Vite config for Render deployment
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // Use root path so assets load correctly on all devices
-  server: {
-    port: 5173, // optional, default dev port
-    strictPort: true, 
-  },
-  build: {
-    outDir: 'dist', // default build output
-  },
+  base: '/',
 })
